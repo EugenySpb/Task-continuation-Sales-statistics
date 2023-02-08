cde88299ea119e02235054a05cf1486bcd16a4b0
EugenySpb <magadanzenit@gmail.com>
2023-02-08 19:36:51 +0300

    Первое добавление программы

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    int[] price = {100, 2000, 3000, 40000, 500, 555, 433, 5554, 4500};
+        SalesManager sales = new SalesManager(price);
+        System.out.printf("Sale max = %,d руб. \n",sales.max());
+        System.out.printf("Sale min = %,d руб. \n",sales.min());
+        System.out.printf("Sale statistics = %,d руб. \n",sales.statistics());
+    }
+}
---
+++ src/SalesManager.java
@@ -0,0 +1,37 @@
+public class SalesManager {
+    protected int[] sales;
+
+    public SalesManager(int[] sales) {
+        this.sales = sales;
+    }
+
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+    }
+
+    public int min() {
+        int min = max();
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public long statistics() {
+        int stat = 0;
+        int j = 0;
+        for (int sale : sales) {
+            stat += sale;
+                j++;
+            }
+        return (stat / j);
+    }
+}
